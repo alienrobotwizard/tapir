b5bfca57d2c2c486044b3cfa73ef7324e6c4e8d0
Jacob Perkins <jacob.a.perkins@gmail.com>
2014-02-04 08:49:26 -0800

    updated generate operator so an "as" clause is allowed to define schema of generated output

--- lib/pigdsl/operators/generate.rb
+++ lib/pigdsl/operators/generate.rb
@@ -1,28 +1,39 @@
 import 'org.apache.pig.newplan.logical.relational.LOGenerate'
 import 'org.apache.pig.newplan.logical.relational.LOInnerLoad'
+import 'org.apache.pig.newplan.logical.relational.LogicalSchema'
+import 'org.apache.pig.impl.logicalLayer.FrontendException'
+import 'org.apache.pig.impl.logicalLayer.schema.Schema'
+import 'org.apache.pig.newplan.logical.Util'
+import 'org.apache.pig.ResourceSchema'
+
+import 'org.codehaus.jackson.map.ObjectMapper'
 
 module LogicalOperator
 
   class Generate < Operator
     attr_accessor :results  # Array of LogicalExpressions to generate
-    attr_accessor :flattens # Array of booleans corresponding to which results to flatten 
+    attr_accessor :flattens # Array of booleans corresponding to which results to flatten
+    attr_accessor :as       # Optional array of schemas corresponding to results
 
-    def initialize results, flattens
+    def initialize results, flattens, as
       @results  = results
       @flattens = flattens
+      @as       = as
     end
 
     def self.from_hash hsh
       results  = hsh[:results].map{|result| LogicalExpression.from_hash(result) }
       flattens = hsh[:flattens]
-      Generate.new(results, flattens)
+      as       = (hsh[:as] || [])
+      Generate.new(results, flattens, as)
     end
 
     def to_hash
       {
         :operator => 'generate',
         :results  => results.map{|result| result.to_hash},
-        :flattens => flattens
+        :flattens => flattens,
+        :as       => as
       }
     end
 
@@ -35,31 +46,55 @@ module LogicalOperator
       gen = LOGenerate.new(current_plan)
 
       input_index = 0
-      plans = results.map do |result|
+      plans = results.map do |result|                
         result.input_index = input_index
         input_index += 1
-        LogicalExpression::Plan.new(pig_context, gen).to_pig(result, in_foreach_plan, nest_context)
+        LogicalExpression::Plan.new(pig_context, gen).to_pig(result, in_foreach_plan, nest_context)        
       end
       
       inner_plan = gen.get_plan
       inputs     = []
 
       # Build inner load statements
+      schemas = []
       plans.each_with_index do |plan, idx|
+        schema = as[idx]
+        expr   = plan.get_sources.first
+        if schema
+          schemas << schema_from_hash(schema)
+        elsif expr.has_field_schema          
+          ls = LogicalSchema.new
+          begin
+            ls.add_field(expr.get_field_schema)
+            schemas << ls
+          rescue FrontendException
+          end         
+        end        
         LogicalOperator.process_expression_plan(current_op, inner_plan, plan, inputs)
       end
       
       gen.set_output_plans(plans)
       gen.set_flatten_flags(flattens.to_java(:boolean))
-      gen.set_user_defined_schema([]) # FIXME: Need to actually handle schema
+      gen.set_user_defined_schema(schemas)
       inner_plan.add(gen) # what witchcraft is this!?
       
       # Connect generate's inputs to it
       inputs.each{|input| inner_plan.connect(input, gen) }
     end
     
+    
+    #
+    # FIXME: Kill this
+    #
+    def schema_from_hash schema
+      json = schema.to_json.to_java(:string) # !
+    
+      rs = (ObjectMapper.new()).readValue(json, ResourceSchema.java_class);
+      Util.translateSchema(Schema.getPigSchema(rs))
+    end
   end
-
+  
+  
   #
   # These are some unpleasant pig functions required for setting up generate
   # operators correctly with inputs
